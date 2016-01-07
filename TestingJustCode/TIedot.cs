7862b9e5355b6f5994a4f255c6f537e29b723d80
jkurkisuonio <jkurkisuonio@gmail.com>
2016-01-07 19:17:29 +0200

    EKa versio

---
+++ TestingJustCode/TIedot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestingJustCode
+{
+    public class Tiedot
+    {
+
+        private string nimi;
+
+        public Tiedot(string nimi)
+        {
+            this.Nimi = nimi;
+        }
+
+        public string Nimi
+        {
+            get
+            {
+                return nimi;
+            }
+
+            set
+            {
+                this.nimi = value;
+            }
+        }
+    }
+}
